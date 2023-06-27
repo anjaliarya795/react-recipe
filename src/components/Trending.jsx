3082e2da86ef48dcafddf7c2ee13a8c98bce52d0
Anjali Arya <anjalisinha498@gmail.com>
2023-06-27 13:50:09 +0530

    styled-components

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import "@splidejs/splide/dist/css/splide.min.css";
+import { Wrapper } from './styled-components/Wrapper'
+import { Card } from './styled-components/Card'
+import { Gradient } from './styled-components/Gradient'
 
 function Trending() {
 
@@ -10,15 +12,25 @@ function Trending() {
 
       useEffect(() => {
         const getTrending = async () => {
-          try {
-            const api = await fetch(
-              `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-            );
-            const data = await api.json();
-            console.log(data);
-            setTrending(data.recipes);
-          } catch (error) {
-            console.log("error fetching data", error);
+
+          const check = localStorage.getItem("trending");
+
+          if(check) {
+            setTrending(JSON.parse(check));
+          }
+
+          else {
+            try {
+              const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=20`
+              );
+              const data = await api.json();
+              setTrending(data.recipes);
+              console.log(data.recipes);
+  
+            } catch (error) {
+              console.log("error fetching data", error);
+            }
           }
         };
       
@@ -55,53 +67,6 @@ function Trending() {
           </Wrapper>
         )
 };    
-
-const Wrapper = styled.div`
-  margin: 4rem 4rem;
-  
-  `;
-
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem ;
-  overflow: hidden;
-  position:relative;
-
-  img {
-    border-radius: 2rem;
-    // width: 25rem;
-    left:0;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
-
-  p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display:flex;
-    justify-content:center;
-    align-items: center;
-  }
-  `;
-
-  const Gradient = styled.div`
-  z-index: 3;
-  position:absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0,0,0,0));
-    
-  `
 
 export default Trending;
 
