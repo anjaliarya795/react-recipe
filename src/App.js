70e2da8cb1e908a72137a2079dec48525e8191e2
Anjali Arya <anjalisinha498@gmail.com>
2023-06-29 23:42:58 +0530

    Search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
+import Search from "./components/Search";
 
 function App() {
   return (
     <BrowserRouter>
 
       <div className="App">
+        
+        <Search/>
         <Category/>
         <Pages/>
+
       </div>
     </BrowserRouter>
 
